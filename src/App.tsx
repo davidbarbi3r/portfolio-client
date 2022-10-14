48367ae9b087378a225545dd758232c6d150c771
davidbarbi3r <davidbarbi3r@gmail.com>
2022-10-14 20:33:04 +0200

    :sparkles: Add blog feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useReducer, useState } from "react";
-import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import { useEffect, useReducer, useState } from "react";
+import { BrowserRouter, NavigateFunction, Route, Routes, useNavigate } from "react-router-dom";
 import Home from "./pages/home";
 import Blog from "./pages/blog";
 import Edit from "./pages/edit";
@@ -14,47 +14,64 @@ import {
 } from "./hooks/userContext";
 import { validate } from "./modules/authServer";
 import logging from "./config/logging";
+import axios, { AxiosResponse } from "axios";
+import config from "./config/config";
+import { IArticleFetched } from "./interfaces/article";
+import Article from "./pages/Article";
 
-type Props = {};
-
-export default function App({}: Props) {
+export default function App() {
   const [userState, userDispatch] = useReducer(userReducer, initialUserState);
   const [loading, setLoading] = useState(true);
   const [authStage, setAuthStage] = useState<string>(
     "Checking localstorage..."
   );
+  const [posts, setPosts] = useState<any[]>([]);
+  const [post, setPost] = useState<IArticleFetched>();
+
+  const getPost = (postId: string, navigate: NavigateFunction) => {
+    console.log(postId);
+    if (postId) {
+      try {
+        axios
+          .get(`${config.server.url}/posts/get/${postId}`)
+          .then((res) => setPost(res.data.post))
+          .then(() => navigate(`/blog/${postId}`))
+
+      } catch {
+        (error: any) => <h2>{error}</h2>;
+      }
+    }
+  };
 
   useEffect(() => {
     setTimeout(() => {
       CheckLocalStorageForCredentials();
     }, 1000);
+    axios
+      .get(`${config.server.url}/posts/get`)
+      .then((res) => setPosts(res.data.posts));
   }, []);
 
   const CheckLocalStorageForCredentials = () => {
-   
     setAuthStage("Checking credentials...");
 
     const fire_token = localStorage.getItem("fire_token");
 
     if (fire_token === null) {
-
       userDispatch({ type: "logout", payload: initialUserState });
       setAuthStage("No credentials found.");
-
     } else {
       //validate with the backend
       return validate(fire_token, (error, user) => {
-  
         if (error) {
           logging.error(error);
-          setAuthStage('Unable to validate user, logging out...')
-          userDispatch({type: "logout", payload: initialUserState})
-   
+          setAuthStage("Unable to validate user, logging out...");
+          userDispatch({ type: "logout", payload: initialUserState });
         } else if (user) {
-          setAuthStage('User authenticated')
-          userDispatch({type: "login", payload: {user, fire_token}});
+          setAuthStage("User authenticated");
+          userDispatch({ type: "login", payload: { user, fire_token } });
         }
-      })
+      });
     }
     setAuthStage("Credentials found, validating");
   };
@@ -78,8 +95,13 @@ export default function App({}: Props) {
                 </AuthRoute>
               }
             />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/blog/:blogID" element={<Blog />} />
+            <Route path="/blog" element={<Blog posts={posts as IArticleFetched[]} getPost={getPost}/>} />
+            <Route
+              path="/blog/:blogID"
+              element={
+                <Article post={post as IArticleFetched}/>
+              }
+            />
             <Route path="/edit" element={<Edit />} />
             <Route path="/edit/:blogID" element={<Edit />} />
             <Route path="/login" element={<Login />} />
---
+++ src/pages/Article.tsx
@@ -0,0 +1,55 @@
+import { IArticleFetched } from '../interfaces/article'
+import Header from "../components/Header";
+import { ThemeContext, LanguageContext } from "../hooks/Context";
+import { useContext } from "react";
+import { styled } from '@stitches/react';
+import { colorTheme } from '../styles/colorTheme';
+
+interface IArticleProps {
+    post: IArticleFetched
+}
+
+export default function Article({post}: IArticleProps) {
+    const { theme } = useContext(ThemeContext);
+    const { language } = useContext(LanguageContext);
+
+    const StyledBlog = styled("section", {
+        backgroundColor: theme ? colorTheme.dark.green6 : colorTheme.light.green1,
+        color: theme ? colorTheme.light.green3 : colorTheme.dark.green3,
+        minHeight: "100vh"
+      });
+ 
+      const StyledArticle = styled("div", {
+        paddingTop: '8em',
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        maxWidth: "1200px"
+      })
+
+  return (
+    <StyledBlog>
+        <Header
+        scroll={() => {
+          ("");
+        }}
+        homeRef={null}
+        aboutRef={null}
+        contactRef={null}
+        projectRef={null}
+      />
+        <StyledArticle>
+            <h2>
+                {post.title}
+            </h2>
+            
+            <p dangerouslySetInnerHTML={{__html: post.body as string}}>
+
+            </p>
+        </StyledArticle>
+                
+    </StyledBlog>
+  )
+}
