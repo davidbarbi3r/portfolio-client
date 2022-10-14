1b7b85cf40f375461783bf26b115ee049d5ab428
davidbarbi3r <davidbarbi3r@gmail.com>
2022-10-14 20:31:57 +0200

    :sparkles: Improve Edit page

--- src/pages/edit.tsx
+++ src/pages/edit.tsx
@@ -11,21 +11,52 @@ import config from "../config/config";
 import { useContext } from "react";
 import UserContext from "../hooks/userContext";
 import logging from "../config/logging";
+import { EditorView } from 'prosemirror-view'
+
 
 const Edit = () => {
-  const auth = getAuth()
   const navigate = useNavigate();
-  const editor = useEditor({
+  
+  const articleTitleEd = useEditor({
     extensions: [StarterKit],
-    content: "<p>Hello World!</p>",
+    content: "<H3>Titre de l'article</h3>",
   });
+
+  const articleInfoEd = useEditor({
+    extensions: [StarterKit],
+    content: "Description de l'article en 2-3 lignes...",
+  });
+
+  const articleTagsEd = useEditor({
+    extensions: [StarterKit],
+    content: "tags de l'article séparés par des => ;",
+  });
+
+  const articleBodyEd = useEditor({
+    extensions: [StarterKit],
+    content: "<p>Corps de l'article</p>",
+  });
+
+  const articleImagesEd = useEditor({
+    extensions: [StarterKit],
+    content: "url des images séparés par des => ;",
+  });
+
   const userContext = useContext(UserContext)
   const {user} = userContext.userState
 
-  function createArticle (html: string):IArticle{
+  const StyledEditPage = {
+    width: "100%",
+    padding: "1em 5em"
+  }
+
+  function createArticle (articleTitle: string | undefined, articleBody: string | undefined, articleDescription: string | undefined):IArticle{
     const article: IArticle = {
-      title: "J'en ai marre des tests",
-      body: html,
+      title: articleTitle,
+      description: articleDescription,
+      tags: articleTags,
+      body: articleBody ? articleBody : "",
+      images: articleImages,
       authorId: user._id,
       date: new Date()
     }
@@ -49,19 +80,28 @@ const Edit = () => {
     }
   }
 
-  const html = editor?.getHTML()
 
-  createArticle(html ? html : "")
+  const articleTitle = articleTitleEd?.getText()
+  const articleInfo = articleInfoEd?.getText()
+  const articleTags = articleTagsEd?.getText()
+  const articleBody = articleBodyEd?.getHTML()
+  const articleImages = articleImagesEd?.getText()
+
+  createArticle(articleTitle, articleBody, articleInfo)
 
   return (
-    <>
+    <div style={StyledEditPage}>
       <h2>EDIT PAGE</h2>
       <h3>La page pour écrire et éditer des articles</h3>
       <button onClick={() => navigate("/")}>Let's go home David</button>
-      <MenuBar editor={editor} />
-      <EditorContent editor={editor} />
-      <button onClick={() => saveArticle(createArticle(html? html : ""))}>Save article</button>
-    </>
+      <EditorContent editor={articleTitleEd}/>
+      <EditorContent editor={articleInfoEd}/>
+      <EditorContent editor={articleTagsEd}/>
+      <MenuBar editor={articleBodyEd} />
+      <EditorContent editor={articleBodyEd} />
+      <EditorContent editor={articleImagesEd}/>
+      <button onClick={() => saveArticle(createArticle(articleTitle, articleBody, articleInfo))}>Save article</button>
+    </div>
   );
 };
 
